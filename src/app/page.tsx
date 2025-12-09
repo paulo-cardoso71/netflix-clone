fe8916b3c3a69ee380b058f2a736080a1b00f022
paulo-cardoso71 <pauloeder7@hotmail.com>
2025-12-09 11:21:30 -0300

    chore: initial infrastructure setup (next15, clerk, prisma5, jotai)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  <h1>Netflix Clone</h1>
+  return (
+    <main className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1 className="text-4xl font-bold text-red-600">Netflix Clone</h1>
+      <p className="text-white">Ambiente Configurado com Sucesso! 🚀</p>
+      
+      <div className="bg-white p-2 rounded-full">
+        <UserButton afterSignOutUrl="/"/>
+      </div>
+    </main>
+  );
 }
